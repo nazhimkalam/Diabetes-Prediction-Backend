f5aa9b50db0daa5f975555fb3e6ce7d07f7dff0f
Nazhim <nazhimkalam@gmail.com>
2021-04-11 10:24:40 +0530

    commit

---
+++ app.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Flask Backend for Diabetes Prediction Modal
+
+# In[5]:
+
+
+# Make sure that all the following modules are already installed for use.
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api, Resource, reqparse
+import joblib
+import numpy as np
+
+
+# In[6]:
+
+
+APP = Flask(__name__)
+APP.config['CORS_HEADERS'] = 'Content-Type'
+CORS(APP)
+API = Api(APP)
+
+
+# In[7]:
+
+
+DIABETIES_PREDICTION_MODEL = joblib.load('diabeties-pred-model.pkl')
+
+
+# In[8]:
+
+
+class Predict(Resource):
+
+    @staticmethod
+    def post():
+        parser = reqparse.RequestParser()
+        parser.add_argument('Pregnancies')
+        parser.add_argument('Glucose')
+        parser.add_argument('BloodPressure')
+        parser.add_argument('SkinThickness')
+        parser.add_argument('Insulin')
+        parser.add_argument('BMI')
+        parser.add_argument('DiabetesPedigreeFunction')
+        parser.add_argument('Age')
+
+        
+        args = parser.parse_args()  # creates dictionary
+
+        X_new = np.fromiter(args.values(), dtype=float)  # convert input to array
+        
+        CATEGORIES = ["Negative", "Positive"]
+
+        out = {'Prediction': CATEGORIES[DIABETIES_PREDICTION_MODEL.predict([X_new])[0]]}
+
+        return out, 200
+
+
+# In[9]:
+
+
+API.add_resource(Predict, '/predict')
+
+if __name__ == '__main__':
+    APP.run(debug=True, port=1080)
+
+
+# In[ ]:
+
+
+import requests
+
+URL = 'http://127.0.0.1:1080/predict'  # localhost and the defined port + endpoint
+
+body = {
+    "Pregnancies": 6,
+    "Glucose": 188,
+    "BloodPressure": 72,
+    "SkinThickness": 35,
+    "Insulin": 168,
+    "BMI": 33.6,
+    "DiabetesPedigreeFunction": 0.627,
+    "Age": 50
+    
+}
+
+response = requests.post(URL, data=body)
+response.json()
+
